50a82ec318cb23198087f9f7c9e52880e6fa9e1b
Misphyr <felipe@wvieira.com.br>
2024-08-09 01:31:30 -0300

    correção da mensagem da página principal...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const HomePage: React.FC = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="bg-gray-900 p-8 rounded-lg shadow-lg w-full max-w-lg outline outline-fuchsia-600 p-2 rounded">
-        <h1 className="text-2xl font-bold">Bem vindo ao minha Toolbox!</h1>
+        <h1 className="text-2xl font-bold">Bem vindo a minha Toolbox!</h1>
         <p className="mt-4">Sinta-se livre para usar qualquer ferramenta.</p>
       </div>
     </div>
